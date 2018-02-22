ef750369d4a44aac4a71066fdcec106cd4a45840
Rodrigo Escobar Gutierrez <rockdrizt@gmail.com>
2018-02-21 18:56:22 -0600

    movil site

---
+++ siteMovil/assets/js/modal.js
@@ -0,0 +1,18 @@
+// Get the modal
+var modal = document.getElementById('modal');
+
+// When the user clicks the button, open the modal 
+$(".plus-btn").click(function() {
+    modal.style.display = "block";
+});
+
+$(".close-btn").click(function() {
+    modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
